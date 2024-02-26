8b3d863ef96baa5c6bb0c0aa2a4ba1aeb4c5fb8c
Keyukemi <keyukemi.ku@gmail.com>
2024-02-26 16:55:08 +0100

    fixed cors issue

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,15 @@ import creditHistoryRouter from "./routes/credithistory.route.js"
 import accountRouter from "./routes/account.route.js"
 import identityRouter from "./routes/identity.route.js"
 
-const app = express()  
+const app = express()
 
 //middleware
 // app.use(cors({origin: "http://localhost:3000"}))
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: 'https://fintrack360.vercel.app'
+}));
 app.use(express.json())
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
@@ -21,23 +23,23 @@ app.use((req, res, next)=>{
 
 //routes
 
-app.use('/api/bvn',bvnRouter)
-app.use('/api/',creditHistoryRouter)
-app.use('/api/',accountRouter)
-app.use('/api/',identityRouter)
+app.use('/api/bvn', bvnRouter)
+app.use('/api/', creditHistoryRouter)
+app.use('/api/', accountRouter)
+app.use('/api/', identityRouter)
 
-app.use("/",(req, res)=>{
+app.use("/", (req, res) => {
   res.send("server is running, mafo")
 });
 
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(()=>{
+  .then(() => {
     console.log('App connected to database');
     app.listen(process.env.PORT, () => {
       console.log('Example app listening on port', process.env.PORT)
     })
   })
-  .catch((error)=>{
+  .catch((error) => {
     console.log(error)
   })
